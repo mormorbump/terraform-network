0d78190c2c5e7b78a7b9a86ad546fc0d70c206dd
kazuki matsumoto <mormorbump@gmail.com>
2020-08-24 23:16:27 +0900

    整理

--- ap-northeast-1/stg/variables.tf
+++ ap-northeast-1/stg/variables.tf
@@ -1,8 +1,2 @@
 variable "name" {}
 variable "region" {}
-variable "profile" {}
-variable "credentials" {}
-variable "ssh_key_name" {}
-variable "backend_s3_name" {}
-variable "ssh_public_key_path" {}
-variable "ssh_private_key_path" {}
