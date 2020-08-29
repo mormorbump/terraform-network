3d5f6b216449885e2ceb169b69c6aa7f4fd45eb4
kazuki matsumoto <mormorbump@gmail.com>
2020-08-30 04:27:51 +0900

    variableの軽微な修正

--- ap-southeast-2/prod/variables.tf
+++ ap-southeast-2/prod/variables.tf
@@ -1,4 +1,6 @@
 variable "name" {}
 variable "region" {}
+variable "aws_credentials_path" {}
+variable "aws_credentials_profile_name" {}
 variable "s3_remote_state_key" {}
 variable "s3_remote_state_bucket" {}
